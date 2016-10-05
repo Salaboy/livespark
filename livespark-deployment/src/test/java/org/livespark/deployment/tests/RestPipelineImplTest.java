7ac3f9082b241f0f25f1bb79e0caffa0185b8297
salaboy <salaboy@gmail.com>
2016-10-05 14:52:13 +0100

    rest test with code server stage

--- livespark-deployment/src/test/java/org/livespark/deployment/tests/RestPipelineImplTest.java
+++ livespark-deployment/src/test/java/org/livespark/deployment/tests/RestPipelineImplTest.java
@@ -10,6 +10,7 @@ import org.apache.commons.io.FileUtils;
 import org.arquillian.cube.CubeController;
 import org.arquillian.cube.HostIp;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
+import org.guvnor.ala.build.maven.config.gwt.GWTCodeServerMavenExecConfig;
 
 import org.guvnor.ala.build.maven.config.MavenBuildConfig;
 import org.guvnor.ala.build.maven.config.MavenBuildExecConfig;
@@ -17,6 +18,7 @@ import org.guvnor.ala.build.maven.config.MavenProjectConfig;
 import org.guvnor.ala.build.maven.executor.MavenBuildConfigExecutor;
 import org.guvnor.ala.build.maven.executor.MavenBuildExecConfigExecutor;
 import org.guvnor.ala.build.maven.executor.MavenProjectConfigExecutor;
+import org.guvnor.ala.build.maven.executor.gwt.GWTCodeServerMavenExecConfigExecutor;
 import org.guvnor.ala.config.BinaryConfig;
 import org.guvnor.ala.config.BuildConfig;
 import org.guvnor.ala.config.ProjectConfig;
@@ -53,6 +55,7 @@ import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 
 /**
  * Test that shows how run Pipeline to provision an app to wildfly
@@ -60,10 +63,8 @@ import org.junit.Before;
 @RunWith( ArquillianConditionalRunner.class )
 public class RestPipelineImplTest {
 
-
     private File tempPath;
-    
-     
+
     @HostIp
     private String ip;
 
@@ -71,8 +72,6 @@ public class RestPipelineImplTest {
     private CubeController cc;
 
     private static final String CONTAINER = "swarm";
-
-    
 
     @Before
     public void setUp() throws IOException {
@@ -83,8 +82,8 @@ public class RestPipelineImplTest {
     public void tearDown() {
         FileUtils.deleteQuietly( tempPath );
     }
-    
-        @Test
+
+    @Test
     @InSequence( 1 )
     public void shouldBeAbleToCreateAndStartTest() {
         cc.create( CONTAINER );
@@ -93,6 +92,7 @@ public class RestPipelineImplTest {
 
     @Test
     @InSequence( 2 )
+    @Ignore
     public void testPipelineForDeployingToWildfly() {
 
         final SourceRegistry sourceRegistry = new InMemorySourceRegistry();
@@ -100,20 +100,19 @@ public class RestPipelineImplTest {
         final InMemoryRuntimeRegistry runtimeRegistry = new InMemoryRuntimeRegistry();
         final WildflyAccessInterface wildflyAccessInterface = new WildflyAccessInterfaceImpl();
 
-        final Stage<Input, SourceConfig> sourceConfig = config( "Git Source", ( s ) -> new GitConfig() {
+        final Stage<Input, SourceConfig> sourceConfig = config( "Git Source", (s) -> new GitConfig() {
         } );
-        final Stage<SourceConfig, ProjectConfig> projectConfig = config( "Maven Project", ( s ) -> new MavenProjectConfig() {
+        final Stage<SourceConfig, ProjectConfig> projectConfig = config( "Maven Project", (s) -> new MavenProjectConfig() {
         } );
-        final Stage<ProjectConfig, BuildConfig> buildConfig = config( "Maven Build Config", ( s ) -> new MavenBuildConfig() {
-        } );
-        
-        final Stage<BuildConfig, BinaryConfig> buildExec = config( "Maven Build", ( s ) -> new MavenBuildExecConfig() {
-        } );
-        final Stage<BinaryConfig, ProviderConfig> providerConfig = config( "Wildfly Provider Config", ( s ) -> new WildflyProviderConfig() {
+        final Stage<ProjectConfig, BuildConfig> buildConfig = config( "Maven Build Config", (s) -> new MavenBuildConfig() {
         } );
 
-       
-        final Stage<ProviderConfig, RuntimeConfig> runtimeExec = config( "Wildfly Runtime Exec", ( s ) -> new ContextAwareWildflyRuntimeExecConfig() );
+        final Stage<BuildConfig, BinaryConfig> buildExec = config( "Maven Build", (s) -> new MavenBuildExecConfig() {
+        } );
+        final Stage<BinaryConfig, ProviderConfig> providerConfig = config( "Wildfly Provider Config", (s) -> new WildflyProviderConfig() {
+        } );
+
+        final Stage<ProviderConfig, RuntimeConfig> runtimeExec = config( "Wildfly Runtime Exec", (s) -> new ContextAwareWildflyRuntimeExecConfig() );
 
         final Pipeline pipe = PipelineFactory
                 .startFrom( sourceConfig )
@@ -124,64 +123,65 @@ public class RestPipelineImplTest {
                 .andThen( runtimeExec ).buildAs( "my pipe" );
         WildflyRuntimeExecExecutor wildflyRuntimeExecExecutor = new WildflyRuntimeExecExecutor( runtimeRegistry, wildflyAccessInterface );
         final PipelineExecutor executor = new PipelineExecutor( asList( new GitConfigExecutor( sourceRegistry ),
-                                                                        new MavenProjectConfigExecutor( sourceRegistry ),
-                                                                        new MavenBuildConfigExecutor(),
-                                                                        new MavenBuildExecConfigExecutor( buildRegistry ),
-                                                                        new WildflyProviderConfigExecutor( runtimeRegistry ),
-                                                                        wildflyRuntimeExecExecutor) );
-        
-       
-        executor.execute( new Input() {{
-            put( "repo-name", "drools-workshop" );
-            put( "branch", "master" );
-            put( "out-dir", tempPath.getAbsolutePath() );
-            put( "origin", "https://github.com/salaboy/drools-workshop" );
-            put( "project-dir", "drools-webapp-example" );
-            put( "wildfly-user", "admin" );
-            put( "wildfly-password", "Admin#70365" );
-            put( "host",  ip);
-            put( "port",  "8080");
-            put( "management-port",  "9990");
-            
-        }}, pipe, System.out::println);
-        
-        List<org.guvnor.ala.runtime.Runtime> allRuntimes = runtimeRegistry.getRuntimes(0, 10, "", true);
+                new MavenProjectConfigExecutor( sourceRegistry ),
+                new MavenBuildConfigExecutor(),
+                new MavenBuildExecConfigExecutor( buildRegistry ),
+                new WildflyProviderConfigExecutor( runtimeRegistry ),
+                wildflyRuntimeExecExecutor ) );
+
+        executor.execute( new Input() {
+            {
+                put( "repo-name", "drools-workshop" );
+                put( "branch", "master" );
+                put( "out-dir", tempPath.getAbsolutePath() );
+                put( "origin", "https://github.com/salaboy/drools-workshop" );
+                put( "project-dir", "drools-webapp-example" );
+                put( "wildfly-user", "admin" );
+                put( "wildfly-password", "Admin#70365" );
+                put( "host", ip );
+                put( "port", "8080" );
+                put( "management-port", "9990" );
+
+            }
+        }, pipe, System.out::println );
+
+        List<org.guvnor.ala.runtime.Runtime> allRuntimes = runtimeRegistry.getRuntimes( 0, 10, "", true );
 
         assertEquals( 1, allRuntimes.size() );
 
         org.guvnor.ala.runtime.Runtime runtime = allRuntimes.get( 0 );
 
-        assertTrue(runtime instanceof WildflyRuntime);
-        
+        assertTrue( runtime instanceof WildflyRuntime );
+
         WildflyRuntime wildflyRuntime = ( WildflyRuntime ) runtime;
-       
+
         WildflyRuntimeManager runtimeManager = new WildflyRuntimeManager( runtimeRegistry, wildflyAccessInterface );
 
         runtimeManager.start( wildflyRuntime );
 
-        allRuntimes = runtimeRegistry.getRuntimes(0, 10, "", true);
+        allRuntimes = runtimeRegistry.getRuntimes( 0, 10, "", true );
 
         assertEquals( 1, allRuntimes.size() );
 
         runtime = allRuntimes.get( 0 );
 
-        assertTrue(runtime instanceof WildflyRuntime);
-        
+        assertTrue( runtime instanceof WildflyRuntime );
+
         wildflyRuntime = ( WildflyRuntime ) runtime;
-        
+
         assertEquals( "Running", wildflyRuntime.getState().getState() );
         runtimeManager.stop( wildflyRuntime );
-        
-        allRuntimes = runtimeRegistry.getRuntimes(0, 10, "", true);
+
+        allRuntimes = runtimeRegistry.getRuntimes( 0, 10, "", true );
 
         assertEquals( 1, allRuntimes.size() );
 
         runtime = allRuntimes.get( 0 );
 
-        assertTrue(runtime instanceof WildflyRuntime);
-        
+        assertTrue( runtime instanceof WildflyRuntime );
+
         wildflyRuntime = ( WildflyRuntime ) runtime;
-        
+
         assertEquals( "Stopped", wildflyRuntime.getState().getState() );
 
         wildflyRuntimeExecExecutor.destroy( wildflyRuntime );
@@ -189,7 +189,76 @@ public class RestPipelineImplTest {
         wildflyAccessInterface.dispose();
 
     }
-    
+
+    @Test
+    @InSequence( 2 )
+    public void testPipelineForDeployingToWildflyWithCodeServer() {
+
+        final SourceRegistry sourceRegistry = new InMemorySourceRegistry();
+        final BuildRegistry buildRegistry = new InMemoryBuildRegistry();
+        final InMemoryRuntimeRegistry runtimeRegistry = new InMemoryRuntimeRegistry();
+        final WildflyAccessInterface wildflyAccessInterface = new WildflyAccessInterfaceImpl();
+
+        final Stage<Input, SourceConfig> sourceConfig = config( "Git Source", (s) -> new GitConfig() {
+        } );
+        final Stage<SourceConfig, ProjectConfig> projectConfig = config( "Maven Project", (s) -> new MavenProjectConfig() {
+        } );
+        final Stage<ProjectConfig, BuildConfig> buildConfig = config( "Maven Build Config", (s) -> new MavenBuildConfig() {
+        } );
+
+        final Stage<BuildConfig, BuildConfig> codeServerExec = config( "Start Code Server", (s) -> new GWTCodeServerMavenExecConfig() {
+        } );
+
+        
+        final Stage<BuildConfig, BinaryConfig> buildExec = config( "Maven Build", (s) -> new MavenBuildExecConfig() {
+        } );
+
+        
+        final Stage<BinaryConfig, ProviderConfig> providerConfig = config( "Wildfly Provider Config", (s) -> new WildflyProviderConfig() {
+        } );
+
+        final Stage<ProviderConfig, RuntimeConfig> runtimeExec = config( "Wildfly Runtime Exec", (s) -> new ContextAwareWildflyRuntimeExecConfig() );
+
+        final Pipeline pipe = PipelineFactory
+                .startFrom( sourceConfig )
+                .andThen( projectConfig )
+                .andThen( buildConfig )
+                .andThen( codeServerExec )
+                .andThen( buildExec )
+                .andThen( providerConfig )
+                .andThen( runtimeExec ).buildAs( "my pipe" );
+        WildflyRuntimeExecExecutor wildflyRuntimeExecExecutor = new WildflyRuntimeExecExecutor( runtimeRegistry, wildflyAccessInterface );
+        final PipelineExecutor executor = new PipelineExecutor( asList( new GitConfigExecutor( sourceRegistry ),
+                new MavenProjectConfigExecutor( sourceRegistry ),
+                new MavenBuildConfigExecutor(),
+                new MavenBuildExecConfigExecutor( buildRegistry ),
+                new GWTCodeServerMavenExecConfigExecutor(),
+                new WildflyProviderConfigExecutor( runtimeRegistry ),
+                wildflyRuntimeExecExecutor ) );
+
+        executor.execute( new Input() {
+            {
+                put( "repo-name", "drools-workshop" );
+                put( "create-repo", "true" );
+                put( "branch", "master" );
+                put( "out-dir", tempPath.getAbsolutePath() );
+                put( "origin", "https://github.com/salaboy/drools-workshop" );
+                put( "project-dir", "drools-webapp-example" );
+                put( "wildfly-user", "admin" );
+                put( "wildfly-password", "Admin#70365" );
+                put( "host", ip );
+                put( "port", "8080" );
+                put( "management-port", "9990" );
+
+            }
+        }, pipe, System.out::println );
+
+        List<org.guvnor.ala.runtime.Runtime> allRuntimes = runtimeRegistry.getRuntimes( 0, 10, "", true );
+
+        wildflyAccessInterface.dispose();
+
+    }
+
     @Test
     @InSequence( 3 )
     public void shouldBeAbleToStopAndDestroyTest() {
